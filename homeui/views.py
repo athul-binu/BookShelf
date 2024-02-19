a4fecf65a1379e00bb9fb0b776c524ec79dce37f
Athul Binu <athul25b@gmail.com>
2024-02-19 19:02:30 +0530

    search feature

--- homeui/views.py
+++ homeui/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render,redirect,HttpResponse
 from .models import category,userModel,bookmodel
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404 
+from django.db.models import Q
 
 
 
 
 def book(request):
     bookdata=bookmodel.objects.all()
+    if request.method=="POST":
+        searchvalue=request.POST.get("search")
+        bookdata=bookmodel.objects.filter(Q(bookname__icontains =searchvalue)|Q(userid__username__icontains=searchvalue))
+        
+        
     return render(request, "book.html",{"data":bookdata})
 
 def mainpage(request):
@@ -20,11 +26,10 @@ def mainpage(request):
     return render(request,"login.html")
     
 def book_details(request, book_id):
-    # Fetch the book object from the database using the book_id
+
     book = get_object_or_404(bookmodel, book_id=book_id)
 
     
-    # Render the book details page with the book object
     return render(request, 'bookdetails.html', {'book': book})
 
 def runcmd(request):
